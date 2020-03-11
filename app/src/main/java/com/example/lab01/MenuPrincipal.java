6a784c1cdc75889dc9c285e6b80223173fed3b22
Diego Babb <diegobabb22@gmail.com>
2020-03-11 12:59:27 -0600

    Clases y evento para el logout

--- app/src/main/java/com/example/lab01/MenuPrincipal.java
+++ app/src/main/java/com/example/lab01/MenuPrincipal.java
@@ -1,13 +1,20 @@
 package com.example.lab01;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.lab01.ui.cursos.CursoFragment;
+import com.example.lab01.ui.login.LoginActivity;
+import com.example.lab01.ui.profesores.ProfesorFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -38,13 +45,30 @@ public class MenuPrincipal extends AppCompatActivity {
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_profesores, R.id.nav_cursos, R.id.nav_logout)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.getMenu().findItem(R.id.nav_logout).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent(MenuPrincipal.this, LoginActivity.class);
+                        startActivity(intent);
+                        return true;
+            }
+        });
+    }
+
+    private void moveToFragment(Fragment fragment) {
+        if(fragment instanceof CursoFragment) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_profesores, fragment, fragment.getClass().getSimpleName()).addToBackStack(null).commit();
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_cursos, fragment, fragment.getClass().getSimpleName()).addToBackStack(null).commit();
+        }
     }
 
     @Override
